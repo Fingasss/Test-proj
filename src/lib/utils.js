c16ce379b0915a0e6dd5fcaf56b6d20f7899dbff
Fingasss <fingassx@gmail.com>
2018-09-17 15:49:52 +0300

    removed console.log

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,7 +1,6 @@
 import { Base64 } from 'js-base64';
 
 export function generateHeader(username, password) {
-  console.log(username, password, `Basic ${Base64.encode(`${username}:${password}`)}`)
   return {
     Authorization: `Basic ${Base64.encode(`${username}:${password}`)}`,
   };
