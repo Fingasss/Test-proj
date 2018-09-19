c496527dee3a14d4bdf710b038273b24338e6ba9
Fingasss <fingassx@gmail.com>
2018-09-19 17:42:18 +0300

    changed input text color

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -63,18 +63,19 @@ button {
   width: 100%;
   outline: none;
   background-color: #2974ff;
+  color: #fff;
   border: none;
-  box-shadow: 0px 3px 8px 0px rgba(0,0,0,0.2);
+  box-shadow: 0px 3px 8px 1px rgba(0,0,0,0.2);
   padding: 8px;
   border-radius: 5px;
   margin-top: 16px;
   cursor: pointer;
   transition: box-shadow .25s linear;
   &:hover {
-    box-shadow: 0px 3px 16px 0px rgba(0,0,0,0.25);
+    box-shadow: 0px 3px 16px 1px rgba(0,0,0,0.25);
   }
   &:active {
-    box-shadow: 0px 3px 8px 0px rgba(0,0,0,0.2);
+    box-shadow: 0px 3px 8px 1px rgba(0,0,0,0.2);
   }
 }
 </style>
